36afa73d30a9879263b9880f1062747e7c151d93
Arthur Alfredo <arthurbalfredo@gmail.com>
2016-09-06 16:20:28 -0300

    Add jquery and events to send and receive messages

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,9 +4,21 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
+let $messages = $('#messages')
+let $input = $('#input')
+
 let channel = socket.channel("room:lobby", {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+
+channel.push("message", {message: "Teste"})
+
+channel.on("message", function(message){
+  $messages.append(`<div class="message">${message.message}</div>`)
+})
+
+
+
 
 export default socket
